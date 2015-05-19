b59c0ac4b875bb9bf01db6bf37c4437000f641f7
Ramon Boss <Ramon.Boss@Swisscom.com>
2015-05-19 08:59:23 +0200

    added empty test class

---
+++ Tests/DeployTest.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @author  List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link    http://swisscom.ch
+ */
+class DeployTest extends \PHPUnit_Framework_TestCase
+{
+  
+}

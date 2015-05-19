fffbcd266f2b2359b3aa465b3c26b7290b0c4e6b
Ramon Boss <Ramon.Boss@Swisscom.com>
2015-05-19 09:28:13 +0200

    added namespace to DeployTest

--- Tests/DeployTest.php
+++ Tests/DeployTest.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Graviton\Deploy\DeployTest;
+
 /**
  * @author  List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License

e69e266ddfffc66b24ffaca570e3fc9d46c75d1a
Ramon Boss <Ramon.Boss@Swisscom.com>
2015-05-21 09:54:07 +0200

    deploy -> deployment

--- src/Deployment.php
+++ src/Deployment.php
@@ -1,16 +1,18 @@
 <?php
+
 /**
  * Class to deploy graviton
  */
 
-namespace Graviton\Deploy;
+namespace Graviton\Deployment;
+
+use Symfony\Component\Process\Process;
 
 /**
  * @author   List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
  * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
  * @link     http://swisscom.ch
  */
-class Deploy
+class Deployment
 {
-  
 }
--- src/Tests/DeploymentTest.php
+++ src/Tests/DeploymentTest.php
@@ -1,23 +1,16 @@
 <?php
+
 /**
  * Validate deploy class
  */
 
-namespace Graviton\Deploy;
+namespace Graviton\Deployment;
 
 /**
  * @author  List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @link    http://swisscom.ch
  */
-class DeployTest extends \PHPUnit_Framework_TestCase
+class DeploymentTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * @return void
-     */
-    public function testInstanceClass()
-    {
-        $deploy = new Deploy();
-        $this->assertInstanceOf('\Graviton\Deploy\Deploy', $deploy);
-    }
 }

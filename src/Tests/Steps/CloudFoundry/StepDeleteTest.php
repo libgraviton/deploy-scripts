a4badb84238483db4ae9e2c39ed80c9464023aa6
lapistano <git@bastian-feder.de>
2015-05-25 00:28:44 +0200

    Added step to delete an application

---
+++ src/Tests/Steps/CloudFoundry/StepDeleteTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Test suite for the delete step.
+ */
+
+namespace Graviton\Deployment\Tests\Steps\CloudFoundry;
+
+use Graviton\Deployment\Steps\CloudFoundry\StepDelete;
+
+/**
+ * @author   List of contributors <https://github.com/libgraviton/graviton/graphs/contributors>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://swisscom.ch
+ */
+class StepDeleteTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Validate getCommand
+     *
+     * @dataProvider provide
+     *
+     * @return void
+     */
+    public function testGetCommand($force, $delMapped, array $flags)
+    {
+        $configuration['cf']['command'] = '/usr/bin/cf';
+        $step = new StepDelete($configuration, 'my_application', 'blue', $force, $delMapped);
+
+        $this->assertEquals(
+            array_merge(array('/usr/bin/cf' , 'delete' , 'my_application-blue'), $flags),
+            $step->getCommand()
+        );
+    }
+
+    public function provide()
+    {
+        return array(
+            'deletemapped routes' => array(false, true, array('-r')),
+            'force delete' => array(true, false, array('-f')),
+            'no force delete' => array(false, false, array()),
+            'force delete and delete all mapped routes' => array(true, true, array('-f', '-r')),
+        );
+    }
+}

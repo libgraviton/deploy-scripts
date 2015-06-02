f41a9753ca76738dae64ab1b058aecc2d9f5fa29
lapistano <git@bastian-feder.de>
2015-06-02 09:41:58 +0200

    Added missing DocBlock

--- src/Tests/Services/CloudFoundry/DeploymentUtilsTest.php
+++ src/Tests/Services/CloudFoundry/DeploymentUtilsTest.php
@@ -37,6 +37,9 @@ class DeploymentUtilsTest extends DeployScriptsTestCase
         );
     }
 
+    /**
+     * @return array
+     */
     public function methodsAndCountsProvider()
     {
         return array(

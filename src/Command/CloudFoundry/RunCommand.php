d742bb8fb55f1c2d0ef20ef22baf6b5c0dd60970
Jan Friedli <jan.friedli@swisscom.com>
2015-08-11 14:42:20 +0200

    use statements added

--- src/Command/CloudFoundry/RunCommand.php
+++ src/Command/CloudFoundry/RunCommand.php
@@ -11,6 +11,11 @@ namespace Graviton\Deployment\Command\CloudFoundry;
 use Graviton\Deployment\Command\AbstractCommand;
 use Graviton\Deployment\Configuration;
 use Graviton\Deployment\Deployment;
+use Graviton\Deployment\Services\CloudFoundry\DeploymentUtils;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
 
 final class RunCommand extends AbstractCommand
 {
@@ -39,9 +44,15 @@ final class RunCommand extends AbstractCommand
             ->setName('graviton:deployment:cf:run')
             ->setDescription('Run a command as a one-off')
             ->addArgument(
-                'Command',
+                'command',
                 InputArgument::REQUIRED,
                 'Command passed as a string example: "console:command -d"'
+            )->addOption(
+                'no-logout',
+                null,
+                InputOption::VALUE_NONE,
+                'Will keep the CF session open after deployment my friend. ' .
+                '<fg=yellow;options=bold>Keep in mind to close it by yourself!</fg=yellow;options=bold>'
             );
     }
 
@@ -56,7 +67,19 @@ final class RunCommand extends AbstractCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         DeploymentUtils::login($this->deployHandler, $output, $this->configuration);
+        DeploymentUtils::runCommand($this->deployHandler, $output, $this->configuration, $input->getArgument('command') );
+        $noLogout = $input->getOption('no-logout');
+        if (true == $noLogout) {
+            $output->writeln(
+                '<bg=yellow;fg=black;options=bold>' .
+                '                                                                           ' . PHP_EOL .
+                '  Cloud Foundry session will not be closed (»no-logout« option was set) !  ' .
+                PHP_EOL . '                                                                           ' .
+                '</bg=yellow;fg=black;options=bold>'
+            );
 
+            return;
+        }
         DeploymentUtils::logout($this->deployHandler, $output, $this->configuration);
     }
 }
